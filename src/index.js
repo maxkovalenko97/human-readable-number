528916c1a743bc86f404761a21eafe23e32f4d75
Max Kovalenko <tutmrmaxi@gmail.com>
2021-08-15 01:52:53 +0300

    feat: add npm tests;

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    const arr1 = [, , 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let arrOfNumber = String(number).split('');
+
+    let result = '';
+
+    if (number > 99) {
+        result += `${arr[arrOfNumber[0]]} hundred `;
+    };
+
+    return result.trim();
 }
